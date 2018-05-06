ea88f675c795b60fdfacf27c1a38ccdfb5ae9f3e
Sitrone <sununiq@163.com>
2018-05-06 10:36:00 +0800

    update notes

---
+++ netty-snippet/src/main/java/com/sununiq/snippet/App.java
@@ -0,0 +1,13 @@
+package com.sununiq.snippet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
