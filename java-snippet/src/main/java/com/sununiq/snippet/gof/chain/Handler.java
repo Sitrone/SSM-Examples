4115ed68158fdf4ab3af850c6253400b1d952375
Sitrone <sununiq@163.com>
2018-07-24 21:23:59 +0800

    add chain proxy example

---
+++ java-snippet/src/main/java/com/sununiq/snippet/gof/chain/Handler.java
@@ -0,0 +1,19 @@
+package com.sununiq.snippet.gof.chain;
+
+import lombok.Setter;
+
+public abstract class Handler {
+
+    @Setter
+    private Handler successor;
+
+    protected abstract void handleProcess();
+
+    public void execute() {
+        handleProcess();
+        if (successor != null) {
+            successor.execute();
+        }
+    }
+
+}
