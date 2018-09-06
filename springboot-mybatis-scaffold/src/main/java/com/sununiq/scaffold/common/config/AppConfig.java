cf8aafd9239a675006860833f0dacd394d97a13a
Sitrone <sununiq@163.com>
2018-09-06 21:17:35 +0800

    add banner examples

--- springboot-mybatis-scaffold/src/main/java/com/sununiq/scaffold/common/config/AppConfig.java
+++ springboot-mybatis-scaffold/src/main/java/com/sununiq/scaffold/common/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.sununiq.scaffold.common.config;
 
 import com.sununiq.scaffold.common.domain.HttpProfile;
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.config.Registry;
@@ -14,12 +15,20 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
+import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @program: springboot-mybatis-scaffold
@@ -77,11 +86,39 @@ public class AppConfig {
 
 	@Bean
 	public RestTemplate restTemplate(HttpClient httpClient) {
+		List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>();
+		interceptors.add(new HeaderRequestInterceptor(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE));
+		interceptors.add(new HeaderRequestInterceptor(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
+
 		RestTemplate restTemplate = new RestTemplate();
 		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 		restTemplate.setRequestFactory(factory);
+
+		// 统一添加http header的两种方式之一，使用interceptor，这个是统一设置
+		// 另一种方式使用HTTPEntity，里面设置HTTPHeader，这个灵活，每个请求可以单独设置
+		restTemplate.setInterceptors(interceptors);
 		return restTemplate;
 	}
 
+
+
+	public class HeaderRequestInterceptor implements ClientHttpRequestInterceptor {
+
+		private final String headerName;
+
+		private final String headerValue;
+
+		public HeaderRequestInterceptor(String headerName, String headerValue) {
+			this.headerName = headerName;
+			this.headerValue = headerValue;
+		}
+
+		@Override
+		public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws
+				IOException {
+			request.getHeaders().set(headerName, headerValue);
+			return execution.execute(request, body);
+		}
+	}
 
 }
