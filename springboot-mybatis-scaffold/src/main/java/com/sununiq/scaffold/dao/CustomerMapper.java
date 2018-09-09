6479e218bd05facbc31e4b1a422b7fcaa0881675
Sitrone <sununiq@163.com>
2018-09-09 14:15:05 +0800

    add mybatis generator examples

---
+++ springboot-mybatis-scaffold/src/main/java/com/sununiq/scaffold/dao/CustomerMapper.java
@@ -0,0 +1,17 @@
+package com.sununiq.scaffold.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @program: springboot-mybatis-scaffold
+ *
+ * @description: 集成mapper分页插件和通用mapper
+ *
+ * @author: sununiq
+ *
+ * @create: 2018-09-06 21:18
+ **/
+@Mapper
+public class CustomerMapper {
+
+}
---
+++ sql-data/readme.md
@@ -0,0 +1,3 @@
+1. MySql 测试sql数据来源:[MySql 官方提供的测试db](https://dev.mysql.com/doc/index-other.html)
+2. MyBatis generator地址：https://github.com/mybatis/generator/releases
+3. 
